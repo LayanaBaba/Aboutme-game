c893e8ea7dcca6f21dac4203360de286630cf231
LayanaBaba <leana_baba@yahoo.com>
2021-03-29 15:02:11 +0300

    It went well, What I learned is very difficult and needs hard work, I expected it to take half an hour, but it took two hours of hard work.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Aboutme-game
+
+*LAB 02*
+
+**About Me, with a guessing game**
+
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app.
+
+**Done By: Layana Baba**
+
+**Links and Resources**
+
+[https://github.com/LTUC/amman-201d20](amman-201d20)
+
+
+**Reflections and Comments**
+
+* >How did this go, overall?
+ It went well.
+
+* >What observations or questions do you have about what you’ve learned so far?
+ What I learned is very difficult and needs hard work.
+
+* >How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+ I expected it to take half an hour, but it took two hours of hard work. 
+
+
+
---
+++ js/app.js
@@ -0,0 +1,138 @@
+'use strict';
+
+let userName=prompt("What's your name?");
+
+alert('Welcome '+userName);
+
+let favColor=prompt('Do I prefer black?');
+
+switch(favColor.toUpperCase()){ 
+    case 'YES':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+   
+    case 'Y':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+
+    case 'NO':
+    console.log('No, that\'s wrong');
+    alert ('No, that\'s wrong');
+    break;
+
+    case 'N':
+        console.log('No, that\'s wrong');
+        alert ('No, that\'s wrong');
+        break;
+}
+
+let favFood=prompt('Do I prefer Mansaf?');
+
+switch(favFood.toUpperCase()){ 
+    case 'YES':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+   
+    case 'Y':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+
+    case 'NO':
+    console.log('No, that\'s wrong');
+    alert ('No, that\'s wrong');
+    break;
+
+    case 'N':
+        console.log('No, that\'s wrong');
+        alert ('No, that\'s wrong');
+        break;
+}
+
+let age=prompt('Am I less than 25 years old?');
+
+switch(age.toUpperCase()){ 
+    case 'YES':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+   
+    case 'Y':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+
+    case 'NO':
+    console.log('No, that\'s wrong');
+    alert ('No, that\'s wrong');
+    break;
+
+    case 'N':
+        console.log('No, that\'s wrong');
+        alert ('No, that\'s wrong');
+        break;
+}
+
+let drivingLicense=prompt('Do I own a driver\'s license?');
+
+switch(drivingLicense.toUpperCase()){ 
+    case 'YES':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+   
+    case 'Y':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+
+    case 'NO':
+    console.log('No, that\'s wrong');
+    alert ('No, that\'s wrong');
+    break;
+
+    case 'N':
+        console.log('No, that\'s wrong');
+        alert ('No, that\'s wrong');
+        break;
+}
+
+let mood=prompt('Am I nervous fast?');
+
+switch(mood.toUpperCase()){ 
+    case 'YES':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+   
+    case 'Y':
+    console.log('Yes, that\'s right');
+    alert ('Yes, that\'s right');
+    break;
+
+    case 'NO':
+    console.log('No, that\'s wrong');
+    alert ('No, that\'s wrong');
+    break;
+
+    case 'N':
+        console.log('No, that\'s wrong');
+        alert ('No, that\'s wrong');
+        break;
+}
+
+
+
+alert('Welcome '+userName);
+
+document.write('Welcome '+userName);
+
+
+
+
+
+
+
